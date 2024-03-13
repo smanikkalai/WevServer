78d4a7bbc63e98b176fc5e5e866a07a994266ba1
selvaraj <105561737+smanikkalai@users.noreply.github.com>
2024-03-13 22:15:38 +0530

    backend updated

--- module.tf
+++ module.tf
@@ -4,8 +4,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.38.0"
+      version = "~> 5.38.0"
     }
+  }
+
+  backend "s3" {
+    bucket         	   = "mycomponents-tfstate"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+    dynamodb_table = "mycomponents_tf_lockid"
   }
 }
 
