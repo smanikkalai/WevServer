40e27488b7de0b2f0bd1eac94a779ae4521a10ec
selvaraj <smanikkalai@presidio.com>
2024-03-14 23:04:48 +0530

    finally

--- module.tf
+++ module.tf
@@ -7,11 +7,6 @@ terraform {
       version = "~> 5.38.0"
     }
   }
-  backend "s3" {
-    bucket = "test19765"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
