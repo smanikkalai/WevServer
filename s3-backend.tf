f9bf62fd9bc1e10581edf7a8dd3a03a4844763bc
selvaraj <smanikkalai@presidio.com>
2024-03-15 23:14:07 +0530

    modified

--- s3-backend.tf
+++ s3-backend.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "test19765"
-#     key    = "Github/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "test19765"
+    key    = "Github/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 
   
