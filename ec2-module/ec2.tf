586db7e8b472462498fa177b4161051471e5fc9b
selvaraj <105561737+smanikkalai@users.noreply.github.com>
2024-03-16 00:36:00 +0530

    destroy

--- ec2-module/ec2.tf
+++ ec2-module/ec2.tf
@@ -7,6 +7,16 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# Generate SSH Key Pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
 
 // creating two instances
 resource "aws_instance" "web_instance" {
@@ -17,34 +27,8 @@ resource "aws_instance" "web_instance" {
   subnet_id                   = element(aws_subnet.subnet[*].id, count.index % length(aws_subnet.subnet[*].id))
   vpc_security_group_ids      = [aws_security_group.sg.id]
   associate_public_ip_address = true
-  user_data = <<EOF
-  #!/bin/bash
-  sudo exec >>(tee -i /var/log/user-data.log)
-  sudo exec 2>&1
-  sudo apt-get update -y 
-  sudo apt-get install software-properties-common -y
-  sudo apt-get install docker -y && sudo apt-get install docker-compose -y
-  sudo usermod -aG docker $USER
-  sudo chmod 777 /var/run/docker.sock
-  mkdir WebServer && cd WebServer
-  git clone https://github.com/evershopcommerce/evershop.git
-  sleep 30
-  docker-compose up -d
-  sleep 45
-  EOF
-
+  user_data                   = templatefile("./script.sh", {})
   tags = var.default_tags
-}
-
-# Generate SSH Key Pair
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "key_pair" {
-  key_name   = "key"
-  public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
 # Output Private Key to a File
--- ec2-module/script.sh
+++ ec2-module/script.sh
@@ -1,13 +1,13 @@
-# #!/bin/bash
-# sudo exec > >(tee -i /var/log/user-data.log)
-# sudo exec 2>&1
-# sudo apt-get update -y 
-# sudo apt-get install software-properties-common -y
-# sudo apt-get install docker -y && sudo apt-get install docker-compose -y
-# sudo usermod -aG docker $USER
-# sudo chmod 777 /var/run/docker.sock
-# mkdir WebServer && cd WebServer
-# git clone https://github.com/evershopcommerce/evershop.git
-# sleep 30
-# docker-compose up -d
-# sleep 45
+#/bin/bash
+sudo exec > > (tee -i /var/log/user-data.log)
+sudo exec 2>&1
+sudo apt-get update -y 
+sudo apt-get install software-properties-common -y
+sudo apt-get install docker -y && sudo apt-get install docker-compose -y
+sudo usermod -aG docker $USER
+sudo chmod 777 /var/run/docker.sock
+mkdir WebServer && cd WebServer
+git clone https://github.com/evershopcommerce/evershop.git
+sleep 30
+docker-compose up -d
+sleep 45
