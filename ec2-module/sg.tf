9114e5db346b9c6f50344c0c90cae0431eb62009
selvaraj <105561737+smanikkalai@users.noreply.github.com>
2024-03-17 12:48:19 +0530

    config done

--- ec2-module/sg.tf
+++ ec2-module/sg.tf
@@ -4,11 +4,6 @@ data "http" "myip" {
   url = "https://ipv4.icanhazip.com"
 }
 
-# output "ip" {
-#   value = data.http.myip.response_body
-# }
-
-
 resource "aws_security_group" "sg" {
   name        = "allow_ssh_http"
   description = "Allow ssh http inbound traffic"
@@ -19,28 +14,18 @@ resource "aws_security_group" "sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["${chomp(data.http.myip.response_body)}/32"]
   }
-
 
   // Ingress rule allowing traffic from local IP
-  ingress {
-    from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
-  
   ingress {
     description      = "HTTP from VPC"
     from_port        = 80
     to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    protocol = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
-
+ 
   egress {
     from_port        = 0
     to_port          = 0
