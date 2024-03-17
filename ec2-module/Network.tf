fe111a25754147a07c72b24cd74518ae599dc0f0
selvaraj <105561737+smanikkalai@users.noreply.github.com>
2024-03-17 12:21:19 +0530

    updated

--- ec2-module/Network.tf
+++ ec2-module/Network.tf
@@ -1,10 +1,14 @@
+
+
+# Network
 ##############################################################################################################################
 # VPC creations
 resource "aws_vpc" "vnet"{
     cidr_block = "10.0.0.0/16"
     tags = var.default_tags
 }
-##############################################################################################################################
+
+
 # SUBNET-creations-PublicA
 resource "aws_subnet" "subnet" {
     count = var.instances_per_subnet
@@ -14,26 +18,31 @@ resource "aws_subnet" "subnet" {
     tags = var.default_tags
 
 }
-##############################################################################################################################
+
+
 # InternetGateWay
 resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.vnet.id
     tags = var.default_tags
 }
-##############################################################################################################################
+
+
 # Elastic-IP
 resource "aws_eip" "eip" {
     depends_on                = [aws_internet_gateway.igw]
     tags = var.default_tags
 }
-##############################################################################################################################
+
+
 # NAT
-resource "aws_nat_gateway" "nat" {
-    allocation_id = aws_eip.eip.id
-    subnet_id = aws_subnet.subnet[0].id
-    tags = var.default_tags
-}
-##############################################################################################################################
+#resource "aws_nat_gateway" "nat" {
+#    allocation_id = aws_eip.eip.id
+#    subnet_id = aws_subnet.subnet[0].id
+#    tags = var.default_tags
+#}
+
+
+
 resource "aws_route_table" "igw-rt" {
     vpc_id = aws_vpc.vnet.id
 
@@ -43,26 +52,31 @@ resource "aws_route_table" "igw-rt" {
     }
     tags = var.default_tags
 }
-##############################################################################################################################
-resource "aws_route_table" "private" {
-    vpc_id = aws_vpc.vnet.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.nat.id
-    }
-    tags = var.default_tags
-}
-##############################################################################################################################
+#
+#resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.vnet.id
+#
+#   route {
+#       cidr_block = "0.0.0.0/0"
+#       nat_gateway_id = aws_nat_gateway.nat.id
+#   }
+#   tags = var.default_tags
+#}
+
+
 #Association Between route-table and subnets
 # aws_subnet_route_table_association
-resource "aws_route_table_association" "associate1" {
+
+resource "aws_route_table_association" "Instance1" {
     subnet_id = aws_subnet.subnet[0].id
     route_table_id = aws_route_table.igw-rt.id
 }
-resource "aws_route_table_association" "associate2" {
+
+resource "aws_route_table_association" "Instance2" {
     subnet_id = aws_subnet.subnet[1].id
     route_table_id = aws_route_table.igw-rt.id
 }
-##############################################################################################################################
+
+
 
