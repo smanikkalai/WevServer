adc0b3aa6388a769dcf07126088c3ad8970aab42
selvaraj <105561737+smanikkalai@users.noreply.github.com>
2024-03-17 12:30:25 +0530

    started

--- ec2-module/sg.tf
+++ ec2-module/sg.tf
@@ -1,19 +1,55 @@
-##############################################################################################################################
+# ##############################################################################################################################
 
-resource "aws_security_group" "sg" {
-  name        = "allow_ssh_http"
-  description = "Allow ssh http inbound traffic"
-  vpc_id      = aws_vpc.vnet.id
+# resource "aws_security_group" "sg" {
+#   name        = "allow_ssh_http"
+#   description = "Allow ssh http inbound traffic"
+#   vpc_id      = aws_vpc.vnet.id
 
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description      = "SSH from VPC"
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
+
+#   // Ingress rule allowing traffic from local IP
+#   ingress {
+#     from_port   = 0
+#     to_port     = 65535
+#     protocol    = "tcp"
+#     cidr_blocks = ["self"]
+#   }
+
+#   ingress {
+#     description      = "HTTP from VPC"
+#     from_port        = 80
+#     to_port          = 80
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+#   tags = var.default_tags
+# }
+# ##############################################################################################################################
+
+
+// testing
+
+resource "aws_security_group" "s3_access" {
+  name        = "s3-access"
+  description = "Security group for S3 access"
+  vpc_id      = aws_vpc.main.id
 
   // Ingress rule allowing traffic from local IP
   ingress {
@@ -23,22 +59,11 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["self"]
   }
 
-  ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
+  // Egress rule allowing all traffic
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = var.default_tags
 }
-##############################################################################################################################
